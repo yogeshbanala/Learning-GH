4d110ba00fc0bbc3d38aaf31d6721b42b188bdac
yogesh banala <yogeshbanala@gmail.com>
2022-11-14 19:17:10 -0800

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Yogesh Banala on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
